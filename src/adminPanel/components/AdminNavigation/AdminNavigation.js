e649533961ab9c1bf0f5888fcd273e24c8181f14
unknown <xahin.mahir@gmail.com>
2022-12-14 23:49:18 +0600

    some changes

--- src/adminPanel/components/AdminNavigation/AdminNavigation.js
+++ src/adminPanel/components/AdminNavigation/AdminNavigation.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './AdminNavigation.css';
 
 const AdminNavigation = () => {
     return (
         <>
-            <center>
-                <Link to='/admin'> Home</Link>
-                <Link to='/admin/homeBanner'> Home Banner</Link>
-                <Link to='/admin/homePortfolio'>Home Portfolio</Link>
-                <Link to='/admin/portfolio'>Portfolio </Link>
-                <Link to='/admin/counter'> Counter</Link>
-                <Link to='/admin/team'> Team</Link>
+            <center className='adminNavContainer'>
+                <Link to='/admin' className='adminNav'> Home</Link>
+                <Link to='/admin/homeBanner' className='adminNav'> Home Banner</Link>
+                <Link to='/admin/homePortfolio' className='adminNav'>Home Portfolio</Link>
+                <Link to='/admin/portfolio' className='adminNav'>Portfolio </Link>
+                <Link to='/admin/counter' className='adminNav'> Counter</Link>
+                <Link to='/admin/team' className='adminNav'> Team</Link>
             </center>
         </>
     );
