a344e4bf380c67daf42f2dc512f340714872f833
unknown <xahin.mahir@gmail.com>
2024-05-11 00:10:49 +0600

    Video Upload

--- src/pages/Home/Video/Video.js
+++ src/pages/Home/Video/Video.js
@@ -4,8 +4,15 @@ import './Video.css';
 const Video = () => {
     return (
         <center className="videoPlayerContainer">
+
+            <iframe className='videoPlayer' src="https://www.youtube.com/embed/gfgZfc8KSE4?si=cKwNUptSQu3dh9mW?rel=0" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+            <br />
+            <br />
+            <br />
+            <br />
             <iframe className='videoPlayer' src="https://www.youtube.com/embed/R0xeocH4lnY?rel=0" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
         </center>
+
     );
 };
 
